d69d0fa5c726530032e20f3563ae6802e2009a55
Spritaro <k5dot.feb@gmail.com>
2021-07-22 07:59:25 +0900

    fix typo

--- scripts/centernet.py
+++ scripts/centernet.py
@@ -249,7 +249,7 @@ class CenterNet(nn.Module):
             num_batch, num_objects, _ = points.shape
             masks = []
             for i in range(num_batch):
-                mask = self.centernet.generate_mask(ctr_logits[i], mask_logits[i], points[0])
+                mask = self.generate_mask(ctr_logits[i], mask_logits[i], points[0])
                 masks.append(mask)
             masks = torch.stack(masks, dim=0)
             return labels, preds, masks
