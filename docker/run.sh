4c82b13f857ed0047900b703483222be794eab67
Spritaro <k5dot.feb@gmail.com>
2021-09-27 10:19:59 +0900

    remove port option from docker run script

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,6 @@ docker run -it --rm \
     -v $1:/home/appuser/condinst_tensorrt:rw \
     -v $2:/home/appuser/dataset:ro \
     -w /home/appuser/condinst_tensorrt \
-    -p 6006:6006 \
     --shm-size 512mb \
     condinst_tensorrt \
     bash
