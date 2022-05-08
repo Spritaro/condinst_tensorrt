ab9dfaf363b4025f75f6140c24f44e6f1c8d1f72
Spritaro <k5dot.feb@gmail.com>
2022-05-08 12:26:30 +0900

    fix encoder initialization

--- scripts/sparseinst.py
+++ scripts/sparseinst.py
@@ -81,7 +81,7 @@ class Encoder(nn.Module):
             if isinstance(m, nn.Conv2d):
                 nn.init.normal_(m.weight, std=0.01)
                 if m.bias is not None:
-                    nn.init.constant_(m.bias, bias)
+                    nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
